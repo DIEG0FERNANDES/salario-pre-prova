0ed29d60beb8345f637fa8c30b8f403219fac174
Diego_dos_Santos_Fernandes <cookieezito@hotmail.com>
2023-05-05 13:24:39 -0400

    1.0.0
    apenas adicionando os arquivos certos

---
+++ src/spec/preProvaSpec.js
@@ -0,0 +1,7 @@
+describe("Suítes de teste para validar o salário a receber seguindo as regras demonstradas acima", function () {
+    it("deve validar o valor do salário visando o total de horas mensais", function(){});
+    it("deve validar o salário somando a hora extra trabalhada", function(){});
+	it("deve validar o salário o  bruto", function(){});
+	it("deve validar o salário total a receber por horas extras", function(){});
+    it("deve validar o salário a receber  equivalente ao salário bruto + quantia a receber por horas extras", function{});
+});
